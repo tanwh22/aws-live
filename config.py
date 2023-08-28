54d2c15e39ea3915c9df93a8faa94b9d577c31a1
tanwh22 <140568681+tanwh22@users.noreply.github.com>
2023-08-28 10:59:03 +0800

    Add files via upload

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-customhost = "database-1.chauwtk5goof.us-east-1.rds.amazonaws.com"
+customhost = "database-1.cdssertij44h.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
-custombucket = "lowchoonkeat-employee1"
+custombucket = "tanweihan-employee"
 customregion = "us-east-1"
 
